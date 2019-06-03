3f38e60ac72b953167d1b56d6aeae3d7c450c9e3
kir4ik <vlados.linkoln@gmail.com>
2019-06-03 15:33:50 +0300

    first commit

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(config.get('mongoURI'), {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+
+        console.log('MongoDB connected...');
+    } catch (error) {
+        console.error(error.message);
+        // Exit process with failure
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
