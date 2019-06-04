01e49b4f2255e0b1e0ba1f602eecb8157eaed17a
kir4ik <vlados.linkoln@gmail.com>
2019-06-04 14:39:29 +0300

    added register

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -17,38 +17,44 @@ const Login = () => {
         console.log('SUCCESS');
     };
 
-    return <Fragment>
-        <h1 className="large text-primary">Sign In</h1>
-        <p className="lead">
-            <i className="fas fa-user" /> Sign Into Your Account
-        </p>
-        <form className="form" onSubmit={e => onSubmit(e)}>
-            <div className="form-group">
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead">
+                <i className="fas fa-user" /> Sign Into Your Account
+            </p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        value={email}
+                        onChange={e => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={e => onChange(e)}
+                        minLength="6"
+                    />
+                </div>
                 <input
-                    type="email"
-                    placeholder="Email Address"
-                    name="email"
-                    value={email}
-                    onChange={e => onChange(e)}
-                    required
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Login"
                 />
-            </div>
-            <div className="form-group">
-                <input
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    value={password}
-                    onChange={e => onChange(e)}
-                    minLength="6"
-                />
-            </div>
-            <input type="submit" className="btn btn-primary" value="Login" />
-        </form>
-        <p className="my-1">
-            Don't have an account? <Link to="/register">Sign Up</Link>
-        </p>
-    </Fragment>;
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+        </Fragment>
+    );
 };
 
 export default Login;
