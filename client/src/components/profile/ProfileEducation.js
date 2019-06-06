ab3225b4dfbd2f93b4688ce0d5f0ba76a6ade644
kir4ik <vlados.linkoln@gmail.com>
2019-06-06 21:04:25 +0300

    added show user's profile by ID, detail info

---
+++ client/src/components/profile/ProfileEducation.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Moment from 'react-moment';
+import PropTypes from 'prop-types';
+
+const ProfileEducation = ({
+  education: { shool, degree, fieldofstudy, from, to, current, description }
+}) => (
+  <div>
+    <h3 className="text-dark">{shool}</h3>
+    <p>
+      <Moment format="YYYY/MM/DD">{from}</Moment> -{' '}
+      {current ? 'Now' : <Moment format="YYYY/MM/DD">{to}</Moment>}
+    </p>
+    <p>
+      <strong>Degree: </strong>
+      {degree}
+    </p>
+    <p>
+      <strong>Field Of Study: </strong>
+      {fieldofstudy}
+    </p>
+    <p>
+      <strong>Description: </strong>
+      {description}
+    </p>
+  </div>
+);
+
+ProfileEducation.propTypes = {
+  education: PropTypes.object.isRequired
+};
+
+export default ProfileEducation;
