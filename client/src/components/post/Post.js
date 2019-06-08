fd588b2c1d0212b8c03d993096efa3cf1bc1a3eb
kir4ik <vlados.linkoln@gmail.com>
2019-06-09 00:21:23 +0300

    Comment Add & show comments of Post & delete comment

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import { getPostById } from '../../actions/post';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
 
 const Post = ({ getPostById, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -19,6 +21,12 @@ const Post = ({ getPostById, post: { post, loading }, match }) => {
         Back to posts
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map(comment => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
